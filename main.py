3a9bbd1661de3337f3c7655e026e532eaafaaa91
Max <79370059+Max835345@users.noreply.github.com>
2024-09-26 13:22:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ paddle_x = W // 2 - paddle_width // 2
 paddle_y = H - paddle_height - 10
 paddle_speed = 10
 
+list_of_block = []
+
 vx = random.randint(-5, 5)
 vy = random.randint(-5, 5)
 
@@ -27,6 +29,36 @@ while True:
     root.fill((255, 255, 255))
     pygame.draw.circle(root, (0, 70, 225), (x, y), r)
     paddle_rect = pygame.draw.rect(root, (0, 0, 0), (paddle_x, paddle_y, paddle_width, paddle_height))
+    list_of_block = [pygame.draw.rect(root, (0, 0, 0), (45, 100, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (100, 100, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (155, 100, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (210, 100, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (265, 100, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (320, 100, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (375, 100, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (430, 100, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (45, 120, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (100, 120, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (155, 120, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (210, 120, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (265, 120, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (320, 120, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (375, 120, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (430, 120, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (45, 140, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (100, 140, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (155, 140, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (210, 140, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (265, 140, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (320, 140, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (375, 140, 50, 15)),
+                     pygame.draw.rect(root, (0, 0, 0), (430, 140, 50, 15)),
+                     ]
+
+    for i in list_of_block:
+        for j in i:
+            pygame.draw.rect(root, (0, 250, 0), i)
+    pygame.display.update()
 
     x += vx
     y += vy
@@ -64,5 +96,9 @@ while True:
         if i.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    for i in range(5):
+        for j in range(5):
+            pygame.draw.rect(root, (0, 0, 0), (i, j, 10, 10))
 
     clock.tick(FPS)
